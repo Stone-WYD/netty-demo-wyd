919a3c055df0bbc3431b5f08d9c2461fd1384ae4
王玉东 <1174872613@qq.com>
2023-01-13 17:31:47 +0800

    粘包半包解决-每个包长度定长

--- src/test/java/com/test/advance/c1/Client2.java
+++ src/test/java/com/test/advance/c1/Client2.java
@@ -1,4 +1,11 @@
 package com.test.advance.c1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Client2 {
+
+    static final Logger log = LoggerFactory.getLogger(Client2.class);
+
+
 }
