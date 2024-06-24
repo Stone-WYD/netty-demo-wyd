aa966dbab8811951c144f7f7b8f82f01df509265
Stone-WYD <1174872613@qq.com>
2024-06-24 14:09:39 +0800

    添加 protobuf 测试内容 总结： 1. protobuf 序列化的时候本身不支持utf8，所以定义为 string 类型的中文字符串在反序列化后直接获取字符串会乱码。 但 protobuf 提供了 ByteString 工具类来解决这个问题。 2. 测试的时候控制台打印内容无论怎么调整，还是会乱码，最终发现是 idea 中没有设置控制台输出内容编码方式

---
+++ src/test/java/com/test/protobuf/Test.java
@@ -0,0 +1,53 @@
+package com.test.protobuf;
+
+import com.google.common.base.Charsets;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.TextFormat;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author xh
+ * @date 2024-06-24
+ * @Description:
+ */
+public class Test {
+    public static byte[] encode(ProtoTest2.Student student) {
+        return student.toByteArray();
+    }
+
+    public static ProtoTest2.Student decode(byte[] bytes) throws InvalidProtocolBufferException {
+        return ProtoTest2.Student.parseFrom(bytes);
+    }
+
+    public static ProtoTest2.Student createStudent() {
+        ProtoTest2.Student.Builder builder = ProtoTest2.Student.newBuilder();
+        builder.setName(ByteString.copyFromUtf8("小明"));
+        builder.setAge(18);
+        // 设置学校
+        ProtoTest2.School.Builder schoolBuilder = ProtoTest2.School.newBuilder();
+        schoolBuilder.setName("宇宙第一中学");
+        schoolBuilder.setNumber(1);
+
+        builder.setSchool(schoolBuilder.build());
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+
+        ProtoTest2.Student student = createStudent();
+        byte[] bytes = encode(student);
+        try {
+            ProtoTest2.Student student1 = decode(bytes);
+            // 打印 byte 数组
+            System.out.println(student1.getName().toStringUtf8());
+            // 打印字符串
+            System.out.println(ByteString.copyFrom(student1.getSchool().getName().getBytes()).toStringUtf8());
+            System.out.println(student1);
+            System.out.println("Assert equal : --> " + student.equals(student1));
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+    }
+}
